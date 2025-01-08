3f7b945e9c01a750b28774eabf97c4dd1016c12b
Anton Liaukevich <leva.dev@gmail.com>
2025-01-08 18:06:10 +0300

    Started to implement DataStoreConfig as implementation of ConfigStorage using DataStore (multiplatform library).

---
+++ composeApp/src/commonMain/kotlin/com/github/saintleva/sourcechew/data/storage/DataStorePreferences.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) Anton Liaukevich 2021-2022 <leva.dev@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.saintleva.sourcechew.data.storage
+
+import androidx.datastore.core.DataMigration
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.Preferences
+import kotlinx.coroutines.CoroutineScope
+
+
+expect fun dataStorePreferences(
+    corruptionHandler: ReplaceFileCorruptionHandler<Preferences>?,
+    coroutineScope: CoroutineScope,
+    migrations: List<DataMigration<Preferences>>,
+    context: Any? = null,
+): DataStore<Preferences>
